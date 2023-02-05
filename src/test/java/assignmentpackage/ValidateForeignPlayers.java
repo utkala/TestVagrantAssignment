84eb869d2b916380aa80444536e14863534061fd
utkala <41109583+utkala@users.noreply.github.com>
2023-02-05 16:15:16 +0530

    Update ValidateForeignPlayers.java

--- src/test/java/assignmentpackage/ValidateForeignPlayers.java
+++ src/test/java/assignmentpackage/ValidateForeignPlayers.java
@@ -2,16 +2,16 @@ package assignmentpackage;
 
 import java.io.IOException;
 
+
 import org.json.simple.parser.ParseException;
 
 public class ValidateForeignPlayers extends Validatejson {
 
 	String names[];
 	String role[];
-	
+		
 	public static void main(String[] args) throws IOException, ParseException {
-		// TODO Auto-generated method stub
-		ValidateForeignPlayers vj = new ValidateForeignPlayers();
+				ValidateForeignPlayers vj = new ValidateForeignPlayers();
           vj.names = vj.readJson();
           vj.validateForeignPlayers(vj.names);
           vj.role = vj.readJsonRole();
